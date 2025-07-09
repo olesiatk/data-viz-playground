d2e2868aa4dd6acd32fb4f59c3ebb734e74c115a
Olesia <okazanivska@leobit.com>
2025-07-10 00:39:14 +0300

    update postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,17 @@
-const prefixSelector = require('postcss-prefix-selector');
+const prefixSelector = require("postcss-prefix-selector");
 
 module.exports = {
-  plugins: [
-    prefixSelector({
-      prefix: '#__prefix',
+  plugins: {
+    [prefixSelector({
+      prefix: "#__prefix",
       transform: (prefix, selector, prefixedSelector) => {
-        if (selector.startsWith('html') || selector.startsWith('body')) {
+        if (selector.startsWith("html") || selector.startsWith("body")) {
           return selector;
         }
         return prefixedSelector;
       },
-    }),
-    require('tailwindcss'),
-    require('autoprefixer'),
-  ],
+    })]: {},
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
